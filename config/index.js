a44b7c5b0184ce1d8b03dc7f78e229fea637458e
sdcxv152 <112227879+sdcxv152@users.noreply.github.com>
2022-08-27 12:01:54 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -5,9 +5,9 @@ export const config = {
     // 公众号appSecret
     appSecret: "5c32124a58d0c2af03609853d960641b",
     // 模板消息id
-    templateId: "LUN2ZullbM1-hfiadw7NhdAcpgdbdMBE1NVJTwQeCnQ",
+    templateId: "OX0BzPcrePZWPJ2h1IyDwzB30Sy8qEGsf6bGH-upKqU",
     // 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如["wx1", "wx2"]
-    user: ["o52La6Sgpp7qwsbuo9CwIoK4qsQE"，"o52La6ZO1ckvQyHD_DT6pAK5v0S8"],
+    user: ["o52La6Sgpp7qwsbuo9CwIoK4qsQE"'"o52La6ZO1ckvQyHD_DT6pAK5v0S8"],
      
     // 信息配置
     // 所在省份
@@ -17,11 +17,12 @@ export const config = {
     // 生日，修改名字为对应需要显示的名字，如果生日为农历， type为 r
     birthdays: [
       {"name": "老婆", "year": "1994", "date": "10-05", "type": 'new'},
+      {"name": "家公", "year": "1999", "date": "12-22", "type": 'new'},
     ],
     // 在一起的日子，格式同上
     loveDate: "2020-07-22",
     // 结婚纪念日
-    marryDate:"2020-07-22"
+    marryDate: "2020-07-22"
     }
 
 // {{date.DATA}}  
